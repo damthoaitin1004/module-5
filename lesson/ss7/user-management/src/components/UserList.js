cddf66357de385a2fade0ead97a5c5eb7417e3a0
damthoaitin1004 <damthoaitin100041997@gmail.com>
2023-08-28 19:49:26 +0700

    nop bai tap

--- lesson/ss7/user-management/src/components/UserList.js
+++ lesson/ss7/user-management/src/components/UserList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useDispatch, useSelector } from "react-redux";
-import { GET_ALL_USER_LIST,DELETE_USER } from '../Types';
+import { GET_ALL_USER_LIST, DELETE_USER } from '../Types';
 
 const UserList = () => {
     const users = useSelector(state => state.userList)
@@ -14,14 +14,15 @@ const UserList = () => {
     const deleteUser = (id) => {
         dispatch({
             type: DELETE_USER,
-            payload: id
+            payload: id,
+            
         })
     }
 
     return (
         <div>
             <h1>User list</h1>
-            <button onClick={() => getUserList()}  type="button">Get users</button>
+            <button onClick={() => getUserList()} type="button">Get users</button>
             <table>
                 <tr>
                     <th>Id</th>
